b6e2663d4d5d09ec5a25cf82579723c5a09d7618
Picard Quentin <quentinp27190@hotmail.fr>
2024-07-14 13:50:28 +0200

    Mise en place du js dom

--- script/main.js
+++ script/main.js
@@ -1,1 +1,17 @@
 lancerJeu ();
+
+let idImputSelection = document.getElementById('inputEcriture');
+console.log(idImputSelection);
+
+let buttonIdSelection = document.getElementById('btnValiderMot');
+console.log(buttonIdSelection);
+
+let zonePropositionSlection = document.querySelector('zoneProposition');
+console.log(zonePropositionSlection);
+
+let zoneScoreSelection = document.querySelector('.zoneScore span');
+console.log(zoneScoreSelection)
+
+let boutonRadioSelection = document.querySelectorAll('.optionSource input')
+console.log(boutonRadioSelection);
+
