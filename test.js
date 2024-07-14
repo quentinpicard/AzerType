53cc93d1792070956227cb5221c692f4419a7ee9
Picard Quentin <quentinp27190@hotmail.fr>
2024-07-14 13:32:47 +0200

    mise en place de l'ui

---
+++ test.js
@@ -0,0 +1,7 @@
+let baliseZoneProposition = document.querySelector("#zoneProposition span"); 
+console.log(baliseZoneProposition)
+
+let listeInputRadio = document.querySelectorAll(".zoneChoix input");
+ for (let i=0; i<listeInputRadio.length; i++){
+    console.log(listeInputRadio[i]);
+ }
