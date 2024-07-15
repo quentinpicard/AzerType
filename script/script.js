070b1f2eb7277e9716964a86ee263e4ab93ff844
Picard Quentin <quentinp27190@hotmail.fr>
2024-07-15 11:11:54 +0200

    Mise en place du choix

--- script/script.js
+++ script/script.js
@@ -13,26 +13,44 @@ function afficherMots (proposition){
 function lancerJeu (){
     let score = 0; 
     let buttonIdSelection = document.getElementById('btnValiderMot');
-    let recupInputEcriture = document.getElementById('inputEcriture')
+    let recupInputEcriture = document.getElementById('inputEcriture');  
+     
     let i = 0; 
-    afficherMots(listeMots[i]);
+    let listeProposition = listeMots;
+
+    afficherMots(listeProposition[i]);
+
+
     buttonIdSelection.addEventListener("click", () => {
-        if (recupInputEcriture.value === listeMots[i] ){
+        if (recupInputEcriture.value === listeProposition[i] ){
             score++;
         }
 
         i++;
-
-        afficherMots(listeMots[i]);
         afficherResultat (score); 
 
         recupInputEcriture.value = '';
-        if (listeMots[i] === undefined){
+        if (listeProposition[i] === undefined){
             afficherMots("Le jeu est fini");
             recupInputEcriture.disabled=  true; 
+        } else {
+            afficherMots(listeProposition[i])
         }
 
-        return i; 
     })
+
+    let radioBox = document.querySelectorAll('input[type=radio]');
+
+    for (let e = 0; e < radioBox.length; e++ ){
+        radioBox[e].addEventListener('change', (event) => {
+            if (event.target.value === "1"){
+                listeProposition = listeMots;
+            }else{
+                listeProposition = listePhrases;
+            }
+            afficherMots(listeProposition[i]);
+        }) 
+    }
+    
 }
 
